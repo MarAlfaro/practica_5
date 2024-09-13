70bf7a2b4f5b875970f6fe4aef6ae7f67a385656
Yancy Alfaro <alfaromargarita556@gmail.com>
2024-09-13 12:01:20 -0600

    Ejercicios completados

---
+++ ejem_if_else.js
@@ -0,0 +1,9 @@
+let puntuacion = 40;
+
+if (puntuacion >= 90){
+    console.log("Aprobaste con A")
+} else if (puntuacion >= 80) {
+    console.log("Aprobaste con B")
+} else {
+    console.log("Aprobaste con C o con menos")
+};
---
+++ ejer_1.js
@@ -0,0 +1,22 @@
+let letraCalificacion = "B";
+
+switch (letraCalificacion) {
+  case "A":
+    console.log("Excelente trabajo");
+    break;
+  case "B":
+    console.log("Buen trabajo");
+    break;
+  case "C":
+    console.log("Es suficiente");
+    break;
+  case "D":
+    console.log("Necesita mejorar");
+    break;
+  case "F":
+    console.log("Reprobado");
+    break;
+  default:
+    console.log("Calificacion no es valida");
+    break;
+}
